2393b0a1a13b1addfa166a58f6e8e2b310ae16a9
ajpicard913 <Contact@saucedesigns.io>
2020-08-16 11:40:39 -0400

    Initial Commit

---
+++ Shared/Refund_DribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Refund_DribbbleApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 8/16/20.
+//
+
+import SwiftUI
+
+@main
+struct Refund_DribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
